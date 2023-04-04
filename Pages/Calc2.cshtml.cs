40be8c20ece587810a035795889beda149d3fc06
info731408@ukr.net <info731408@ukr.net>
2023-04-04 02:55:08 +0200

    Add project files.

---
+++ Pages/Calc2.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Pages
+{
+    public class CalcModel2 : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public CalcModel2(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
