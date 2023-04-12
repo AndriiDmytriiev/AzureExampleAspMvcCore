1d753e4d2b3bbc0c89e8e75083710a5d61fda788
info731408@ukr.net <info731408@ukr.net>
2023-04-12 23:32:24 +0200

    Update

--- Pages/Calc1.cshtml.cs
+++ Pages/Calc1.cshtml.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace WebApplication2.Pages
 {
@@ -14,12 +16,66 @@ namespace WebApplication2.Pages
 
         public CalcModel(ILogger<IndexModel> logger)
         {
+            var connectionString = "Server=tcp:server14235.database.windows.net,1433;Initial Catalog=newdb;Persist Security Info=False;User ID=sa124312;Password=123123Ye$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var strQuery = "SELECT [Body] FROM [dbo].[Articles] where ID=1";
+                conn.Open();
+                SqlCommand command1 = new SqlCommand(strQuery, conn);
+                var cmdSelectFromProduct = command1.ExecuteScalar();
+
+                System.Data.DataTable table = new System.Data.DataTable();
+
+
+
+
+                SqlDataReader dr = command1.ExecuteReader();
+                //cmbRefCpv.Items.Add("All");
+                //cmbRefCpv.Text = "All";
+                //chkAllRefCpv.Checked = true;
+
+                while (dr.Read())
+                {
+                    //clbRefCpv.Items.Add(dr["REFERENCED_CPV"].ToString());
+                    ViewData["result"] = dr[0].ToString();
+                }
+
+                dr.Close();
+                conn.Close();
+            }
             _logger = logger;
         }
 
         public void OnGet()
         {
+            var connectionString = "Server=tcp:server14235.database.windows.net,1433;Initial Catalog=newdb;Persist Security Info=False;User ID=sa124312;Password=123123Ye$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var strQuery = "SELECT [Body] FROM [dbo].[Articles] where ID=1";
+                conn.Open();
+                SqlCommand command1 = new SqlCommand(strQuery, conn);
+                var cmdSelectFromProduct = command1.ExecuteScalar();
 
+                System.Data.DataTable table = new System.Data.DataTable();
+
+
+                
+
+                SqlDataReader dr = command1.ExecuteReader();
+                //cmbRefCpv.Items.Add("All");
+                //cmbRefCpv.Text = "All";
+                //chkAllRefCpv.Checked = true;
+
+                while (dr.Read())
+                {
+                    //clbRefCpv.Items.Add(dr["REFERENCED_CPV"].ToString());
+                    ViewData["result"] = dr[0].ToString();
+                }
+
+                dr.Close();
+                conn.Close();
+            }
+            //ViewData["result"] = "Calculator 1";
         }
     }
 }
